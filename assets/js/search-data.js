a65074093920c3e5ea8c01b9a6e557e6ba1a0e85
yanyan-dong <yanyandong@arizona.edu>
2026-02-18 22:54:50 +0000

    Deploying to gh-pages from @ yanyan-dong/yanyan-dong.github.io@e541c4cd9cb8b37f820dfdff1a25d6f822ccf6db 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "dropdown-favorites",
               title: "Favorites",
               description: "",
